7f5200d2a69bb07e5295578ca5271f2cb5b70588
bon-com <iwatamasateru1111@gmail.com>
2023-09-14 20:26:49 +0900

    MessageSourceをコンテキストローダー側へ移動（アプリケーション全体で使用するため）

--- src/main/java/com/example/myapp/config/AppConfig.java
+++ src/main/java/com/example/myapp/config/AppConfig.java
@@ -1,11 +1,26 @@
 package com.example.myapp.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 /**
  * Bean定義を行う
  */
 @Configuration
 public class AppConfig {
-
+    
+	/**
+	 * Beanバリデーションメッセージ等を制御する。
+	 * MessageSourceはメッセージの格納先を抽象化する。
+	 * 
+	 * @return
+	 */
+	@Bean
+	public MessageSource messageSource() {
+	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	    messageSource.setBasenames("application-message", "message");
+	    return messageSource;
+	}
 }
--- src/main/java/com/example/myapp/config/WebMvcConfig.java
+++ src/main/java/com/example/myapp/config/WebMvcConfig.java
@@ -1,10 +1,7 @@
 package com.example.myapp.config;
 
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -28,18 +25,5 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     	// WEB-INF/views配下のjspとマッピング
         registry.jsp("/WEB-INF/views/", ".jsp");
     }
-    
-	/**
-	 * Beanバリデーションメッセージ等を制御する。
-	 * MessageSourceはメッセージの格納先を抽象化する。
-	 * 
-	 * @return
-	 */
-	@Bean
-	public MessageSource messageSource() {
-	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-	    messageSource.setBasenames("application-message", "message");
-	    return messageSource;
-	}
 
 }
